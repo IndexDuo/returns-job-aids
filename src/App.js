9d8e5a835509234828f569ffacd601156693bf82
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-07-13 06:56:27 +0000

    Jul 13, 2024, 2:56 AM

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import AgentInfo from "./components/AgentInfo.js";
 
 function App() {
     return (
-        <div>
+        <div className="container">
             <header>
-                <h1>Email Template Assistance</h1>
+                {/* <h1>Email Template Assistance</h1> */}
             </header>
-            <main>
-                <AgentInfo />
-                <EmailTemplate />
+            <main className="row">
+                <div className="col-md-1">
+                    <AgentInfo />
+                </div>
+                <div className="col-md-15">
+                    <EmailTemplate />
+                </div>
             </main>
             <footer>
                 <p>&copy; 2024 Returns Assistance</p>
