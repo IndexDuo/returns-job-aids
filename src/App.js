eba4fdca2f86c813629d54bc41622285de6fa21e
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-07-13 07:33:44 +0000

    Jul 13, 2024, 3:33 AM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import EmailTemplate from "./components/EmailTemplate.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AgentInfo from "./components/AgentInfo.js";
 
 function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        checkPassword();
+    }, []);
+
+    const checkPassword = () => {
+        const password = prompt("Please enter the password:", "");
+        if (password === "123") {
+            setIsAuthenticated(true);
+        } else {
+            alert("Incorrect password. Please try again.");
+            checkPassword();
+        }
+    };
+
+    if (!isAuthenticated) {
+        return null; // Or you could return a loading spinner here
+    }
+
     return (
         <div className="container">
             <header>{/* <h1>Email Template Assistance</h1> */}</header>
