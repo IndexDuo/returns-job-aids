f0b44425ab7928bb38ee63b28abf4ce790c3040b
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-07-15 00:41:30 +0000

    Jul 14, 2024, 8:41 PM

--- src/components/ReturnsGuide.js
+++ src/components/ReturnsGuide.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 
 const ReturnsGuide = () => {
     return (
-        <div className="mt-5">
+        <div className="mt-5 border p-3 rounded">
             <h2>Returns Guide</h2>
             <p>Here is the guide for returns.</p>
         </div>
