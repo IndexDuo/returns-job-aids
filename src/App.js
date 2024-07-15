b8fef0045648db24a6209ced968eadc051174464
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-07-15 04:25:14 +0000

    Jul 15, 2024, 12:25 AM

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
     };
     const handleSelectTemplate = (template) => {
         setSelectedTemplate(template);
+        console.log("selectedTemplate: " + template);
     };
 
     if (!isAuthenticated) {
