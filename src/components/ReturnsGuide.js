d78cf7de0ee4d5e336b4bee604bef81f7909cecc
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-07-18 05:02:26 -0400

    chore: Refactor ReturnsGuide component to use useState and useEffect

--- src/components/ReturnsGuide.js
+++ src/components/ReturnsGuide.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 
 const ReturnsGuide = (selectedTemplate) => {
-    const template = JSON.parse(selectedTemplate.selectedTemplate);
-    //console.log(template.guide);
+    const [template, setTemplate] = useState("");
+
+    useEffect(() => {
+        if (selectedTemplate) {
+            const template2 = JSON.parse(selectedTemplate.selectedTemplate);
+            setTemplate(template2);
+            // console.log(template2);
+        }
+    }, [selectedTemplate]);
+
+    useEffect(() => {
+        const template2 = JSON.parse(selectedTemplate.selectedTemplate);
+        setTemplate(template2);
+        // console.log(template2);
+    }, []);
+
+    // console.log(template);
     return (
         <div className="mt-5 border p-3 rounded">
             <h2>Template Guide</h2>
-            <p>{template.name}</p>
-            <p>{template.guide}</p>
+            <p>{template ? template.name : ""}</p>
+            <p>{template ? template.guide : ""}</p>
         </div>
     );
 };
